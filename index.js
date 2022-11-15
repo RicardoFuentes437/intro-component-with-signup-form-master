234579ebc597328b7415446cd6e4bdae8dd430c4
Ricardo Fuentes Urbina <ricardo.furbina@alumnos.udg.mx>
2022-11-14 18:23:20 -0600

    avance responsiveness

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ let fnMessage = document.querySelector('.fn-message');
 let lnMessage = document.querySelector('.ln-message');
 let eMessage = document.querySelector('.e-message');
 let pMessage = document.querySelector('.p-message');
-const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+let firstInput = document.querySelector('.first-name-input');
+let lastInput = document.querySelector('.last-name-input');
+let emailInput = document.querySelector('.email-input');
+let passInput = document.querySelector('.password-input');
+const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
@@ -15,18 +19,33 @@ form.addEventListener('submit', function(event){
 
     if(!fnInput){
         fnMessage.style.opacity = 1;
+        firstInput.style.backgroundImage = "url('images/icon-error.svg')";
+        firstInput.style.backgroundRepeat = "no-repeat";
+        firstInput.style.backgroundPosition = "90% 50%";
     }
 
     if(!lnInput){
         lnMessage.style.opacity = 1;
+        lastInput.style.backgroundImage = "url('images/icon-error.svg')";
+        lastInput.style.backgroundRepeat = "no-repeat";
+        lastInput.style.backgroundPosition = "90% 50%";
     }
 
     if(regex.test(eInput) == false || !eInput){
         eMessage.style.opacity = 1;
+        emailInput.style.backgroundImage = "url('images/icon-error.svg')";
+        emailInput.style.backgroundRepeat = "no-repeat";
+        emailInput.style.backgroundPosition = "90% 50%";
     }
 
     if(!pInput){
         pMessage.style.opacity = 1;
+        passInput.style.backgroundImage = "url('images/icon-error.svg')";
+        passInput.style.backgroundRepeat = "no-repeat";
+        passInput.style.backgroundPosition = "90% 50%";
     }
 
+    if((fnInput.length > 0) && (lnInput.length > 0) && (eInput.length > 0 && regex.test(eInput) != false) && (pInput.length > 0)){
+        window.location.reload();
+    }
 })
